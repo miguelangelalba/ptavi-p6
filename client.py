877d312a367c60edb5793db046363ab442a1876b
Miguel Ngel Alba Blanco <maalbab@gamma01.aulas.gsyc.urjc.es>
2017-11-20 10:53:28 +0100

    Estracción de datos de los argumentos y definición de metodo msg_creator

--- client.py
+++ client.py
@@ -5,26 +5,39 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
-
+def msg_constructor(sip_type):
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+def comunication (server,port,sip_type,login):
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+    #SERVER = 'localhost'
+    #PORT = 6001
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+    # Contenido que vamos a enviar
+    LINE = '¡Hola mundo!'
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((SERVER, PORT))
 
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
+        print("Enviando: " + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
 
-print("Fin.")
+        print('Recibido -- ', data.decode('utf-8'))
+        print("Terminando socket...")
+
+    print("Fin.")
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
+
+    sip_type = sys.argv[1]
+    login = sys.argv[2][:sys.argv[2].find("@")]
+    server = sys.argv[2][sys.argv[2].find("@") + 1:sys.argv[2].find(":")]
+    port = sys.argv[2][sys.argv[2].find(":") + 1:]
