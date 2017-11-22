988fac792c0c5e86734d2bc1fc730c358718985a
miguelangel3 <miguelangel.abb@gmail.com>
2017-11-22 20:56:24 +0100

    Estoy con el esqueleto del programa y definiendo mi estructura de datos, tengoq que ver con que diccionario me quedo

--- server.py
+++ server.py
@@ -5,13 +5,26 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
+answer_code = {
+    "Trying":"SIP/2.0 100 Trying\r\n",
+    "Ringing":"SIP/2.0 180 Ringing\r\n",
+    "Ok":"SIP/2.0 200 OK\r\n",
+    "Bad Request":"SIP/2.0 400 Bad Request\r\n",
+    "Methon Not Allowed":"SIP/2.0 405 Method Not Allowed\r\n"}
+
+#SIP_type = {
+#    "INVITE":"SIP/2.0 100 Trying\r\n" + "SIP/2.0 180 Ringing\r\n" +
+#            "SIP/2.0 200 OK\r\n"
+#    "BYE":
+#    "ACK":
+#}
 
 class SIPServer(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
@@ -23,6 +36,11 @@ class SIPServer(socketserver.DatagramRequestHandler):
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+    def msg_answer_constructor(self,sip_type):
+        if sip_type == "INVITE":
+            pass
+    pass
+
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
@@ -31,5 +49,5 @@ if __name__ == "__main__":
     port = int(sys.argv[2])
     audio = sys.argv[3]
     serv = socketserver.UDPServer((sys.argv[1], port), SIPServer)
-    print("Lanzando servidor UDP de eco...")
+    print("Listening...")
     serv.serve_forever()
