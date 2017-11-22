0ca746667c5aee6caa10e34b5eb70d4f2da3c0b2
miguelangel3 <miguelangel.abb@gmail.com>
2017-11-22 12:47:57 +0100

    Creación de mensajes invite y bye, controlo que sean siempre en mayusculas y se envían correctamente al servidor en formato SIP

--- client.py
+++ client.py
@@ -8,23 +8,23 @@ import socket
 import sys
 
 # Cliente UDP simple.
-def msg_constructor(sip_type):
+def msg_constructor(sip_type,login):
 # Dirección IP del servidor.
+    msg_to_send = sip_type + " sip:" + login + " SIP/2.0" + "\r\n"
+    return msg_to_send
+
 def comunication (server,port,sip_type,login):
-
-    #SERVER = 'localhost'
-    #PORT = 6001
-
     # Contenido que vamos a enviar
     LINE = '¡Hola mundo!'
 
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        my_socket.connect((SERVER, PORT))
+        my_socket.connect((server, port))
 
         print("Enviando: " + LINE)
-        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        msg_to_send = msg_constructor(sip_type,login)
+        my_socket.send(bytes(msg_to_send, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
 
         print('Recibido -- ', data.decode('utf-8'))
@@ -37,7 +37,8 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
-    sip_type = sys.argv[1]
+    sip_type = sys.argv[1].upper()
     login = sys.argv[2][:sys.argv[2].find("@")]
     server = sys.argv[2][sys.argv[2].find("@") + 1:sys.argv[2].find(":")]
-    port = sys.argv[2][sys.argv[2].find(":") + 1:]
+    port = int(sys.argv[2][sys.argv[2].find(":") + 1:])
+    comunication(server,port,sip_type,login)
